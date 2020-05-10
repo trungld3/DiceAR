cdde5b74d3c561a9eed370bd59ef11bbf4c863f7
trungld3 <k21tpm4@gmail.com>
2020-05-10 23:42:44 +0700

    roll all

--- ARDice1/ViewController.swift
+++ ARDice1/ViewController.swift
@@ -11,6 +11,9 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    
+    var diceArray = [SCNNode]()
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -79,21 +82,40 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                             diceNode.position = SCNVector3( hitResult.worldTransform.columns.3.x,
                                                             hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius
                                 , hitResult.worldTransform.columns.3.z)
+                            
+                            
+                            diceArray.append(diceNode)
                             sceneView.scene.rootNode.addChildNode(diceNode)
                             
-                            
-                            let radomX = Float (arc4random_uniform(4) + 1 ) * (Float.pi/2)
-                            let radomZ = Float( arc4random_uniform(4) + 1) * (Float.pi/2)
-                            
-                            diceNode.runAction(SCNAction.rotateBy(
-                                x: CGFloat(radomX * 5)
-                                , y: 0,
-                                  z: CGFloat(radomZ * 5),
-                                  duration: 0.5)
-                            )
+                            roll(dice: diceNode)
+                           
                         }
             }
         }
+    }
+    func rollAll(){
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                roll(dice : dice)
+            }
+        }
+    }
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        rollAll()
+    }
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollAll()
+    }
+    func roll(dice : SCNNode) {
+        let radomX = Float (arc4random_uniform(4) + 1 ) * (Float.pi/2)
+                                   let radomZ = Float( arc4random_uniform(4) + 1) * (Float.pi/2)
+                                   
+                                   dice.runAction(SCNAction.rotateBy(
+                                       x: CGFloat(radomX * 5)
+                                       , y: 0,
+                                         z: CGFloat(radomZ * 5),
+                                         duration: 0.5)
+                                   )
     }
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         if anchor is ARPlaneAnchor {
